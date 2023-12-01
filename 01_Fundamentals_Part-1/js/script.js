4d314eaa33985b1da245fd5b8e1c5ba31ddd97bf
Serhii Shabala <sergei181989@gmail.com>
2023-12-01 15:58:51 +0200

    :bookmark: Fundamentals part-1 v1

---
+++ 01_Fundamentals_Part-1/js/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+console.log('----< script.js >----');
+
+// 009 => Linking a JavaScript File.
+
+// let js = 'amazing';
+
+// console.log(40 + 8 + 23 - 10);
+
+// 010 => Values and Variables.
+
+// let firstName = 'Jonas';
+// console.log(firstName);
+
+// let jonas_matilda = 'JM';
+// let $function = 27;
+
+// let person = 'jonas';
+// let PI = 3.14159;
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Gamer and Coder';
+
+// 012 => Data Types.
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year); // undefined
+
+// year = 1991;
+// console.log(typeof year); // number
+
+// console.log(typeof null); // object
