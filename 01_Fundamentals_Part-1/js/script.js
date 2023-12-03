31796be6b8a12dac42763e2a0887889f384f1f51
Serhii Shabala <sergei181989@gmail.com>
2023-12-03 14:57:12 +0200

    :bookmark: Check chellenge #1

--- 01_Fundamentals_Part-1/js/script.js
+++ 01_Fundamentals_Part-1/js/script.js
@@ -34,3 +34,73 @@ console.log('----< script.js >----');
 // console.log(typeof year); // number
 
 // console.log(typeof null); // object
+
+// 013 => let, const and var.
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+
+// 014 => Basic Operators.
+
+// Math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2020;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// const firstName = 'Jonas';
+// const lastName = 'Doe';
+
+// console.log(firstName + ' ' + lastName);
+
+// // Assignment operators
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+
+// console.log(x);
+
+// // Comparison operators
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// 015 => Operator Precedence.
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2020;
+
+// let x, y;
+
+// x = y = 25 - 10 - 5; // x = y = 10
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
+// 016 => Coding Challenge #1.
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJhon = 92;
+// const heightJhon = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJhon = 85;
+// const heightJhon = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJhon = massJhon / heightJhon ** 2;
+// console.log(BMIMark, BMIJhon);
+
+// const markHeherBMI = BMIMark > BMIJhon;
+// console.log(markHeherBMI);
