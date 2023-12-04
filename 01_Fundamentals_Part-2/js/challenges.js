e364a63c92ecd22fc866f777db62942bb533bad2
Serhii Shabala <sergei181989@gmail.com>
2023-12-04 23:58:57 +0200

    :bookmark: Check chellenge #2

--- 01_Fundamentals_Part-2/js/challenges.js
+++ 01_Fundamentals_Part-2/js/challenges.js
@@ -22,3 +22,21 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 
 checkWinner(teamDolphinsDataOne, teamKoalasDataOne);
 checkWinner(teamDolphinsDataTwo, teamKoalasDataTwo);
+
+// * Challenge #2
+
+const calcTip = function (value) {
+  if (value <= 300 && value >= 50) {
+    return value * 0.15;
+  } else {
+    return value * 0.2;
+  }
+};
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(calcTip(100));
+console.log(tips);
+console.log(total);
--- 01_Fundamentals_Part-2/js/script.js
+++ 01_Fundamentals_Part-2/js/script.js
@@ -82,4 +82,41 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 };
 
 console.log(yearsUntilRetirement(1991, 'Alex'));
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const years1 = new Array(1991, 1984, 2003, 2020);
+console.log(years1);
+
+console.log(friends.at(-1));
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1956, 2003, 2010, 2018];
+
+console.log(calcAge(years[0]));
+console.log(calcAge(years[1]));
+console.log(calcAge(years[years.length - 1]));
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years.at(-1))];
+console.log(ages);
+
+const friends = ['Michael', 'Steven', 'Peter'];
+friends.push('Jay');
+console.log(friends);
+
+console.log(friends.indexOf('Peter')); // 2 return position in array or -1 
+console.log(friends.indexOf('Bob')); // -1
+
+console.log(friends.includes('Peter')); // true (return true or false) Проверяет строгим сравнением
+
+if (friends.includes('Peter')) {
+  console.log('You have a friend called Peter');
+}
 */
