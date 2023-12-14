4948b4061c73e71f56356dc3e8c9911aa6c91515
Serhii Shabala <sergei181989@gmail.com>
2023-12-14 21:49:02 +0200

    :bookmark: Behind the scenes v1.1

--- 08_Behind-the-Scenes/js/script.js
+++ 08_Behind-the-Scenes/js/script.js
@@ -23,4 +23,41 @@ function calcAge(birthYear) {
 
 const firstName = 'John';
 calcAge(1991);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAge(1991);
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(2037 - this.year);
+
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+  greet: () => console.log(`Hey ${this.firstName}`), // undefined, this.firstName === window.firstName
+};
+
+jonas.calcAge();
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const a = { age: 18 };
+const aCopy = Object.assign({}, a);
+aCopy.age = 20;
+aCopy.year = 2037;
+
+console.log(a, aCopy);
 */
