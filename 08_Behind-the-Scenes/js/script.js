3b289a00dc96eaacdfdef12706ea2778bdcdd5a4
Serhii Shabala <sergei181989@gmail.com>
2023-12-14 18:18:50 +0200

    :bookmark: Behind the scenes v1.0

--- 08_Behind-the-Scenes/js/script.js
+++ 08_Behind-the-Scenes/js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+/*
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1991 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+
+    console.log(millenial);
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = 'John';
+calcAge(1991);
+*/
