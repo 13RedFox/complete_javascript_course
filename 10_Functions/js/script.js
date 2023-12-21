e0fea0f3c8e338f04d5dc5f35c3f521d84032362
Serhii Shabala <sergei181989@gmail.com>
2023-12-21 16:15:15 +0200

    Functions v1.2

--- 10_Functions/js/script.js
+++ 10_Functions/js/script.js
@@ -16,9 +16,9 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 
 createBooking('LH123');
 createBooking('LH123', 2, 800);
-
 */
 
+/*
 const flight = 'LH234';
 const john = {
   name: 'John',
@@ -43,3 +43,100 @@ const newPassport = function (person) {
 
 newPassport(john);
 checkIn(flight, john);
+*/
+
+/*
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str, fn) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function:
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time.
+const high5 = function () {
+  console.log('🏆');
+};
+
+['John', 'Samanta', 'Alex'].forEach(high5);
+*/
+
+/*
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Alex');
+
+greet('Hello')('Alex');
+
+// Challenge:
+
+const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+greetArr('Hello')('Smith..');
+
+*/
+
+/*
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`,
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(293, 'Alex Smith');
+lufthansa.book(391, 'John Doe');
+
+const eurowings = {
+  airline: 'EuroWings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Call method:
+book.call(eurowings, 23, 'Sarah Williams');
+
+book.call(lufthansa, 13, 'Mary Cooper');
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+
+// Apply method:
+const fligthData = [583, 'George Cooper'];
+book.apply(swiss, fligthData);
+
+book.call(swiss, ...fligthData);
+
+console.log(swiss);
+*/
